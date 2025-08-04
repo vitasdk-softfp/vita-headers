6f503a17fcbd9d48ac4f13876ba461c8c56a7855
Volodymyr Atamanenko <v@atamanenko.in>
2025-08-04 10:40:41 +0400

    Update download_external_libs.sh

--- .travis.d/download_external_libs.sh
+++ .travis.d/download_external_libs.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
-curl -sL https://github.com/vitasdk/vdpm/raw/master/vdpm -o vdpm
+curl -sL https://github.com/vitasdk-softfp/vdpm/raw/master/vdpm -o vdpm
 chmod +x vdpm
-curl -L https://github.com/vitasdk/vdpm/raw/master/include/install-packages.sh -o install-packages.sh
+curl -L https://github.com/vitasdk-softfp/vdpm/raw/master/include/install-packages.sh -o install-packages.sh
 sed -i "s/\.\.\///" install-packages.sh
-curl -L https://github.com/vitasdk/vdpm/raw/master/install-all.sh | sed -e "s/include\///" | bash > /dev/null
+curl -L https://github.com/vitasdk-softfp/vdpm/raw/master/install-all.sh | sed -e "s/include\///" | bash > /dev/null
 rm vdpm
